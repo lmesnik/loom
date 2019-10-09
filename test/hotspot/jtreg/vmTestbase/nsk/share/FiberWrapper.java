94b6d705e152bb79388141884c015b31859ad947
lmesnik <leonid.mesnik@oracle.com>
2019-10-09 12:01:53 -0700

    JDI wrapper added.

---
+++ test/hotspot/jtreg/vmTestbase/nsk/share/FiberWrapper.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013, 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package nsk.share;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Predicate;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+
+
+
+public final class FiberWrapper {
+
+    public FiberWrapper() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        String className = args[0];
+        String[] classArgs = new String[args.length - 1];
+        System.arraycopy(args, 1, classArgs, 0, args.length - 1);
+        Class c = Class.forName(className);
+        Method mainMethod = c.getMethod("main", new Class[] { String[].class });
+
+
+        //mainMethod.invoke(null, new Object[] { classArgs });
+        Fiber fiber = FiberScope.background().schedule(() -> {
+                //System.out.println("Running test in fiber: " + className);
+                //                new Exception().printStackTrace(System.out);
+                mainMethod.invoke(null, new Object[] { classArgs });
+                return null;
+            });
+        fiber.join();
+
+    }
+}
